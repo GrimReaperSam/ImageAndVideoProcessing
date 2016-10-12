6ca5ca6b186e44d7aa826236cfab37991543a4d9
christophetrinh <christophe.trinh.94@gmail.com>
2016-10-12 10:27:08 +0200

    add part 4-5

--- Lab2/main.m
+++ Lab2/main.m
@@ -74,6 +74,97 @@ title('Image dithered with a ordered threshold method')
 
 %% Exercise 4 - Ordered matrix with centered points
 lena = imread('Images/lena-y.png');
+wool = imread('Images/wool.png');
+
+lena_quant = quantize(lena,37);
+wool_quant = quantize(wool,37);
+
+thresh_mat_C6 = [34 25 21 17 29 33;
+                30 13 9 5 12 24;
+                18 6 1 0 8 20;
+                22 10 2 3 4 16;
+                26 14 7 11 15 28;
+                35 31 19 23 27 32];
+            
+thresh_mat_E6 = [30 22 16 21 33 35;
+                    24 11 7 9 26 28;
+                    13 5 0 2 14 19;
+                    15 3 1 4 12 18;
+                    27 8 6 10 25 29;
+                    32 30 17 23 31 34];
+                
+lena_dith_ord_C6 = threshold_fct(lena_quant,thresh_mat_C6);
+wool_dith_ord_C6 = threshold_fct(wool_quant,thresh_mat_C6);
+
+lena_dith_ord_E6 = threshold_fct(lena_quant,thresh_mat_E6);
+wool_dith_ord_E6 = threshold_fct(wool_quant,thresh_mat_E6);
+
+
+figure('Name','Ordered threshold method with centered points - Lena');
+subplot(1,3,1)
+imshow(lena,[])
+title('Original image')
+subplot(1,3,2)
+imshow(lena_dith_ord_C6,[])
+title('Image dithered with C6')
+
+subplot(1,3,3)
+imshow(lena_dith_ord_E6,[])
+title('Image dithered with E6')
+
+
+figure('Name','Ordered threshold method with centered points - Wool');
+subplot(1,3,1)
+imshow(wool,[])
+title('Original image')
+subplot(1,3,2)
+imshow(wool_dith_ord_C6,[])
+title('Image dithered with C6')
+
+subplot(1,3,3)
+imshow(wool_dith_ord_E6,[])
+title('Image dithered with E6')
+
+%% Exercise 5 Diagonal ordered matrix with balanced centered points
+
+lena = imread('Images/lena-y.png');
+wool = imread('Images/wool.png');
+
+lena_quant = quantize(lena,32);
+wool_quant = quantize(wool,32);
+
+O_8_1 = [13 9 5 12;
+           6 1 0 8;
+           10 2 3 4;
+           14 7 11 15];
+       
+O_8_2 = [18 22 26 19;
+        25 30 31 23;
+        21 29 28 27;
+        17 24 20 16];
+thresh_mat_O8 = [O_8_1 O_8_2;
+        O_8_2 O_8_1];
+    
+ 
+lena_dith_ord_O8 = threshold_fct(lena_quant,thresh_mat_O8);
+wool_dith_ord_O8 = threshold_fct(wool_quant,thresh_mat_O8);
+
+figure('Name','Diagonal ordered matrix with balenced centered points')
+subplot(2,2,1)
+imshow(lena,[])
+title('Original image')
+subplot(2,2,2)
+imshow(lena_dith_ord_O8)
+title('Image dithered with O8')
+
+subplot(2,2,3)
+imshow(wool,[])
+title('Original image')
+subplot(2,2,4)
+imshow(wool_dith_ord_O8)
+title('Image dithered with O8')
+
+
 
 
 
