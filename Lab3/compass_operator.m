7da18d90081e4e3f370f92dd42b47293ec6d9137
christophetrinh <christophe.trinh.94@gmail.com>
2016-11-09 10:02:00 +0100

    update compass and main

--- Lab3/compass_operator.m
+++ Lab3/compass_operator.m
@@ -1,4 +1,4 @@
-function [ results ] = compass_operator(img,ind)
+function [ results ] = compass_operator(img)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,10 +6,17 @@ function [ results ] = compass_operator(img,ind)
             -3 0 5;
             -3 -3 5];
         
-    image = double(img);
-    kangle = imrotate(k0,45*ind,'crop');
+    image = im2double(img);
     
-    results = conv2(image,kangle,'same');
+    for index =1:8
+        kangle = imrotate(k0,45*(index-1),'crop');
+        if index == 1
+            img_compassed =conv2(image,kangle,'same');
+        else
+            img_compassed = cat(3, conv2(image,kangle,'same'),img_compassed);
+        end
+    
+    results = max(img_compassed,[],3);
     
 end
 
