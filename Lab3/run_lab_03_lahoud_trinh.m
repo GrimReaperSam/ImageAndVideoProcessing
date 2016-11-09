b40bdb117524ced7ad900ca78b4168c7575aa032
christophetrinh <christophe.trinh.94@gmail.com>
2016-11-09 11:57:14 +0100

    final main

--- Lab3/run_lab_03_lahoud_trinh.m
+++ Lab3/run_lab_03_lahoud_trinh.m
@@ -13,7 +13,6 @@ demo_template_edge(lena, 'Lena');
 demo_template_edge(rice, 'Rice');
 demo_template_edge(road, 'Road');
 
-
 %% Exercise 2 - Compass operator
 lena = imread('Images/lena.png');
 rice = imread('Images/rice.png');
@@ -36,6 +35,7 @@ title('Rice')
 subplot(1,3,3)
 imshow(road_compassed, [])
 title('Road')
+
 
 
 %% Exercise 3 - Laplace operator
@@ -70,16 +70,170 @@ lena_grad = frei_chen_edge(lena, 0.9);
 rice_grad = frei_chen_edge(rice, 0.9);
 road_grad = frei_chen_edge(road, 0.9);
 
-figure('name', 'Frei-Chen method')
+figure('name', 'Laplace operator')
 subplot(1, 3, 1)
-imshow(lena_grad, [])
+imshow(lena_grad)
 title('Lena')
 
 subplot(1, 3, 2)
-imshow(rice_grad, [])
+imshow(rice_grad)
 title('Rice')
 
 subplot(1, 3, 3)
-imshow(road_grad, [])
+imshow(road_grad)
 title('Road')
+
+
+%% Exercice 5 - Evaluation
+
+lena = imread('Images/lena.png');
+lena= im2double(lena);
+lena_noisy_5 = imnoise(lena,'gaussian',0,(5/255)^2); 
+lena_noisy_11 = imnoise(lena,'gaussian',0,(11/255)^2);
+lena_noisy_25 = imnoise(lena,'gaussian',0,(25/255)^2);
+    
+lena_noisy_5_sobel_l2   = template_edge(lena_noisy_5, 'Sobel', 0.2, 'L2');
+lena_noisy_5_sobel_l1   = template_edge(lena_noisy_5, 'Sobel', 0.2, 'L1');
+lena_noisy_11_sobel_l2   = template_edge(lena_noisy_11, 'Sobel', 0.2, 'L2');
+lena_noisy_11_sobel_l1   = template_edge(lena_noisy_11, 'Sobel', 0.2, 'L1');
+lena_noisy_25_sobel_l2   = template_edge(lena_noisy_11, 'Sobel', 0.2, 'L2');
+lena_noisy_25_sobel_l1   = template_edge(lena_noisy_11, 'Sobel', 0.2, 'L1');
+
+figure('name', 'lena noisy, Sobel');
+subplot(2, 3, 1)
+imshow(lena_noisy_5_sobel_l2,[])
+title('std=5 - Sobel L2')
+subplot(2, 3, 2)
+imshow(lena_noisy_5_sobel_l1,[])
+title('std=5 - Sobel L1')
+
+subplot(2, 3, 3)
+imshow(lena_noisy_11_sobel_l2,[])
+title('std=11 - Sobel L2')
+subplot(2, 3, 4)
+imshow(lena_noisy_11_sobel_l1,[])
+title('std=11 - Sobel L1')
+
+subplot(2, 3, 5)
+imshow(lena_noisy_25_sobel_l2,[])
+title('std=25 - Sobel L2')
+subplot(2, 3, 6)
+imshow(lena_noisy_25_sobel_l1,[])
+title('std=25 - Sobel L1')
+
+
+lena_noisy_5_prewitt_l2   = template_edge(lena_noisy_5, 'Prewitt', 0.2, 'L2');
+lena_noisy_5_prewitt_l1   = template_edge(lena_noisy_5, 'Prewitt', 0.2, 'L1');
+lena_noisy_11_prewitt_l2   = template_edge(lena_noisy_11, 'Prewitt', 0.2, 'L2');
+lena_noisy_11_prewitt_l1   = template_edge(lena_noisy_11, 'Prewitt', 0.2, 'L1');
+lena_noisy_25_prewitt_l2   = template_edge(lena_noisy_11, 'Prewitt', 0.2, 'L2');
+lena_noisy_25_prewitt_l1   = template_edge(lena_noisy_11, 'Prewitt', 0.2, 'L1');
+
+figure('name', 'lena noisy, Prewitt');
+subplot(2, 3, 1)
+imshow(lena_noisy_5_prewitt_l2,[])
+title('std=5 - Prewitt L2')
+subplot(2, 3, 2)
+imshow(lena_noisy_5_prewitt_l1,[])
+title('std=5 - Prewitt L1')
+
+subplot(2, 3, 3)
+imshow(lena_noisy_11_prewitt_l2,[])
+title('std=11 - Prewitt L2')
+subplot(2, 3, 4)
+imshow(lena_noisy_11_prewitt_l1,[])
+title('std=11 - Prewitt L1')
+
+subplot(2, 3, 5)
+imshow(lena_noisy_25_prewitt_l2,[])
+title('std=25 - Prewitt L2')
+subplot(2, 3, 6)
+imshow(lena_noisy_25_prewitt_l1,[])
+title('std=25 - Prewitt L1')
+
+
+lena_noisy_5_roberts_l2   = template_edge(lena_noisy_5, 'Roberts', 0.3, 'L2');
+lena_noisy_5_roberts_l1   = template_edge(lena_noisy_5, 'Roberts', 0.3, 'L1');
+lena_noisy_11_roberts_l2   = template_edge(lena_noisy_11, 'Roberts', 0.3, 'L2');
+lena_noisy_11_roberts_l1   = template_edge(lena_noisy_11, 'Roberts', 0.3, 'L1');
+lena_noisy_25_roberts_l2   = template_edge(lena_noisy_11, 'Roberts', 0.3, 'L2');
+lena_noisy_25_roberts_l1   = template_edge(lena_noisy_11, 'Roberts', 0.3, 'L1');
+
+figure('name', 'lena noisy, Roberts');
+subplot(2, 3, 1)
+imshow(lena_noisy_5_roberts_l2,[])
+title('std=5 - Roberts L2')
+subplot(2, 3, 2)
+imshow(lena_noisy_5_roberts_l1,[])
+title('std=5 - Roberts L1')
+
+subplot(2, 3, 3)
+imshow(lena_noisy_11_roberts_l2,[])
+title('std=11 - Roberts L2')
+subplot(2, 3, 4)
+imshow(lena_noisy_11_roberts_l1,[])
+title('std=11 - Roberts L1')
+
+subplot(2, 3, 5)
+imshow(lena_noisy_25_roberts_l2,[])
+title('std=25 - Roberts L2')
+subplot(2, 3, 6)
+imshow(lena_noisy_25_roberts_l1,[])
+title('std=25 - Roberts L1')
+
+
+lena_noisy_5_compassed = compass_operator(lena_noisy_5,0.7);
+lena_noisy_11_compassed = compass_operator(lena_noisy_11,0.7);
+lena_noisy_25_compassed = compass_operator(lena_noisy_25,0.7);
+
+figure('name', 'lena noisy, Compass operator');
+subplot(1,3,1)
+imshow(lena_noisy_5_compassed,[])
+title('Lena noisy std = 5')
+subplot(1, 3, 2)
+imshow(lena_noisy_11_compassed,[])
+title('Lena noisy std = 11')
+subplot(1, 3, 3)
+imshow(lena_noisy_25_compassed,[])
+title('Lena noisy std = 25')
+
+
+lena = imread('Images/lena.png');
+lena_noisy_5 = imnoise(lena,'gaussian',0,(5/255)^2); 
+lena_noisy_11 = imnoise(lena,'gaussian',0,(11/255)^2);
+lena_noisy_25 = imnoise(lena,'gaussian',0,(25/255)^2);
+lena_noisy_5_laplacian = laplacian_edge(lena_noisy_5, 3, 0.3);
+lena_noisy_11_laplacian = laplacian_edge(lena_noisy_11, 3, 0.3);
+lena_noisy_25_laplacian = laplacian_edge(lena_noisy_25, 3, 0.3);
+
+figure('name', 'lena noisy, Laplace operator');
+subplot(1,3,1)
+imshow(lena_noisy_5_laplacian)
+title('Lena noisy std = 5')
+subplot(1, 3, 2)
+imshow(lena_noisy_11_laplacian,[])
+title('Lena noisy std = 11')
+subplot(1, 3, 3)
+imshow(lena_noisy_25_laplacian,[])
+title('Lena noisy std = 25')
+
+lena = imread('Images/lena.png');
+lena_noisy_5 = imnoise(lena,'gaussian',0,(5/255)^2); 
+lena_noisy_11 = imnoise(lena,'gaussian',0,(11/255)^2);
+lena_noisy_25 = imnoise(lena,'gaussian',0,(25/255)^2);
+lena_noisy_5_frei_chen = frei_chen_edge(lena_noisy_5, 0.9);
+lena_noisy_11_frei_chen = frei_chen_edge(lena_noisy_11, 0.9);
+lena_noisy_25_frei_chen = frei_chen_edge(lena_noisy_25, 0.9);
+
+figure('name', 'lena noisy, Frei Chen operator');
+subplot(1,3,1)
+imshow(lena_noisy_5_frei_chen,[])
+title('Lena noisy std = 5')
+subplot(1, 3, 2)
+imshow(lena_noisy_11_frei_chen,[])
+title('Lena noisy std = 11')
+subplot(1, 3, 3)
+imshow(lena_noisy_25_frei_chen,[])
+title('Lena noisy std = 25')
+
 
