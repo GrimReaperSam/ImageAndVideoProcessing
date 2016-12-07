fb0c072329744d93c90877722efd2588114dde8e
Christophe Trinh <christophe.trinh.94@gmail.com>
2016-12-07 11:39:02 +0100

    plot objectives metrics

--- Lab5/run_lab_05_lahoud_trinh.m
+++ Lab5/run_lab_05_lahoud_trinh.m
@@ -8,14 +8,16 @@ load('data/content_lut.mat')
 load('data/bitrate_lut.mat')
 load('data/bitrate_values.mat')
 load('data/raw_scores.mat')
-load('data/metrics.mat')
+metrics = load('data/metrics.mat');
 
 codecs = {'JPEG', 'HEVC', 'JPEG 2000 (PSNR)', 'JPEG 2000 (visual)', 'Daala',...
     'JPEG XR (444)', 'WebP', 'JPEG (PSNR)', 'JPEG (visual)', 'JPEG XR (420)'};
 contents = {'bike', 'cafe', 'honolulu_zoo', 'p08', 'p26', 'woman'};
 bitrates = {'R1', 'R2', 'R3', 'R4'};
-
 % Lab 05 - Fayez LAHOUD, Christophe TRINH - 23th November 2016
+
+
+
 %% Plotting MOS values with CI for each content
 [mos, ci] = MOS(raw_scores);
 content_types = unique(content_lut);
@@ -38,4 +40,36 @@ for i=1:length(content_types)
     legend('location', 'southeast')
     legend('boxoff')
 end
+
+
+%% Plotting objectives metrics
+
+name_metrics = fieldnames(metrics);
+content_types = unique(content_lut);
+codec_types = unique(codec_lut);
+
+
+for i=1:length(content_types)
+    content = content_types(i);
+    indices = find(content_lut == content);
+    figure('Name', contents{i})
+    for j=1:length(name_metrics)
+        for index_codec = 1:length(codec_types)
+            codec = codec_types(index_codec);
+            codec_indices = find(codec_lut == codec);
+            bit_indices = intersect(indices, codec_indices);
+            metrics_values = getfield(metrics,name_metrics{j});
+            subplot(3,3,j)
+            plot(bitrate_values(bit_indices),metrics_values(bit_indices),'DisplayName',codecs{index_codec});
+            hold on
+        end
+        hold off
+        xlabel('Bitrates')
+        ylabel(strrep(name_metrics{j},'_',''))
+    end
+    legend('show')
+    legend('location', 'bestoutside')
+    legend('boxoff')
+end
+
 
